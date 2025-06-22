a64f9e1f4660494ad4704c5706b8acd2e8b34143
hr-freitas <psartes@gmail.com>
2025-06-21 22:32:06 -0300

    exercice2_calculadora

---
+++ script.js
@@ -0,0 +1,29 @@
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
+
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        let value = e.target.innerHTML;
+
+        if (value === '=') {
+            try {
+                string = math.evaluate(string);
+                input.value = string;
+            } catch (error) {
+                input.value = "Erro";
+                string = "";
+            }
+        } else if (value === 'AC') {
+            string = "";
+            input.value = string;
+        } else if (value === 'DEL') {
+            string = string.slice(0, -1);
+            input.value = string;
+        } else {
+            string += value;
+            input.value = string;
+        }
+    });
+});
